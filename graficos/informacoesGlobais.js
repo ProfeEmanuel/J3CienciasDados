89d7c3b9c60f92680b5ce2b98e6ca403e181b144
ProfeEmanuel <140930070+ProfeEmanuel@users.noreply.github.com>
2024-11-07 23:52:31 +0000

    reuniao 6

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,15 +3,22 @@ const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGr
 async function vizualizarInformacoesGlobais() {
 const res = await fetch(url)
 const dados = await res.json()
-console.log(dados)
+const pesssoasConectadas = (dados.total_pessoas_com_acesso_a_educacao/1e9)
+const pessoasNoMundo = (dados.total_pessoas_mundo/1e9)
+const horas = parseInt (dados.tempo_medio_dia_estudando)
+const minutos = Math.round((dados.tempo_medio_dia_estudando - horas)*60)
+const pessoasSuperior = (dados.total_pessoas_com_educacao_superior/1e9)
+const porcentagemConectada = ((pesssoasConectadas/pessoasNoMundo)*100).toFixed(2)
 
-const paragrafo = document.createElement('p');
-paragrafo.classList.add('graficos-container__texto');
-paragrafo.innerHTML = `Você sabia que o mundo tem 7.888.000.000 de pessoas?<span> 
-${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente <span> ${dados.total_pessoas_empregadas} </span> estão empregadas em serviços registrados. Eles trabalham <span> ${dados.tempo_medio_trabalho_por_semana} </span> horas por semana.`
-const container = document.getElementById('graficos-container')
-container.app
+const paragrafo = document.createElement('p')
+paragrafo.classList.add('graficos-container__texto')
+paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span>
+de pessoas?
+e que aproximadamente <span>${pesssoasConectadas} bilhões</span> tem acesso à educação
+e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> estudando. 
+Além disto, cerca de <span>${pessoasSuperior} bilhões</span> tem educação superior.<br> Isso significa que aproximadamente <span>${porcentagemConectada}%</span> pessoas conectadas em alguma rede social`
+
+const container = document.getElementById('graficos-container');
 container.appendChild(paragrafo);
 }
-}
-vizualizarInformacoesGlobais()
+vizualizarInformacoesGlobais();
